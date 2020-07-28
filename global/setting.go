87d00b9f13b3818689fdf83f7b6e3ce5b3b60cfe
baibai <569531262@qq.com>
2020-07-28 23:02:22 +0800

    全局变量

--- global/setting.go
+++ global/setting.go
@@ -1,1 +1,11 @@
 package global
+import "blog-service/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	EmailSetting    *setting.EmailSettingS
+	JWTSetting      *setting.JWTSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+	//Logger          *logger.Logger
+)
